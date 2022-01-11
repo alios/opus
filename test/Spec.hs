fa11458977e638bf38cf654739b1e03681e005f9
Yuto Takano <moa17stock@gmail.com>
2022-01-11 21:36:28 +0000

    Add stereo test vector tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -38,10 +38,10 @@ onlyIfOpusCompareExists action = do
     Right (ExitFailure 1, _, _) -> action
     _ -> fail "opus_compare executable not found"
 
-decodeFile :: Decoder -> B.ByteString -> IO B.ByteString
-decodeFile decoder bytes = do
+decodeFile :: DecoderConfig -> B.ByteString -> IO B.ByteString
+decodeFile decoderCfg bytes = do
   decoder <- opusDecoderCreate decoderCfg
-  loop bytes
+  loop decoder bytes
   where
 
     -- | Convert four unsigned bytes to a 32-bit integer. fromIntegral is
@@ -50,15 +50,12 @@ decodeFile decoder bytes = do
     charToInt (b1:b2:b3:b4:[]) = (fromIntegral b1) `shiftL` 24 .|. (fromIntegral b2) `shiftL` 16 .|. (fromIntegral b3) `shiftL` 8 .|. (fromIntegral b4)
     charToInt _ = error "wrong length to convert to int"
 
-    decoderCfg :: DecoderConfig
-    decoderCfg = _DecoderConfig # (opusSR48k, False)
-
     maxPacket, maxFrameSize :: Int
     maxPacket = 1500
     maxFrameSize = 48000 * 2
 
     -- | A simplified port of the official opus_demo.c file's decoding loop
-    loop bytes
+    loop decoder bytes
       | B.length bytes < 8 = pure mempty
       | otherwise = do
         -- lines 649 to 672 in opus_demo.c
@@ -75,7 +72,7 @@ decodeFile decoder bytes = do
         let outputSamples = maxFrameSize
         decoded <- opusDecode decoder (_DecoderStreamConfig # (decoderCfg, outputSamples, 0)) inputData
         -- recursively continue with the rest
-        (decoded <>) <$> loop remaining
+        (decoded <>) <$> loop decoder remaining
 
 main :: IO ()
 main = hspec $ do
@@ -85,11 +82,11 @@ main = hspec $ do
     -- These tests require the opus_compare executable to be present in the
     -- project root, and the opus test vectors, downloaded from the official
     -- opus website.
-    describe "opus mono test vectors" $ do
+    describe "opus mono test vectors" $
       forM_ ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"] $ \file -> do
-        it ("Testing testvector " <> file) $ do
-          decoder <- opusDecoderCreate (_DecoderConfig # (opusSR48k, False))
-          B.readFile ("opus_newvectors/testvector" <> file <> ".bit") >>= decodeFile decoder >>= B.writeFile "tmp.out"
+        it ("mono testvector " <> file) $ do
+          let decoderCfg = _DecoderConfig # (opusSR48k, False)
+          B.readFile ("opus_newvectors/testvector" <> file <> ".bit") >>= decodeFile decoderCfg >>= B.writeFile "tmp.out"
           -- Use readProcessWithExitCode to account for the fact that opus_compare
           -- returns a non-zero exit code if the comparing fails.
           (exitcode1, stdout1, error1) <- readProcessWithExitCode "./opus_compare"
@@ -99,6 +96,27 @@ main = hspec $ do
             ] ""
           (exitcode2, stdout2, error2) <- readProcessWithExitCode "./opus_compare"
             ["-r", "48000"
+            , "opus_newvectors/testvector" <> file <> "m.dec"
+            , "tmp.out"
+            ] ""
+          shouldSatisfy (error1, error2) $ \(a, b) -> "PASSES" `isInfixOf` a || "PASSES" `isInfixOf` b
+
+    describe "opus stereo test vectors" $
+      forM_ ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"] $ \file -> do
+        it ("stereo testvector " <> file) $ do
+          let decoderCfg = _DecoderConfig # (opusSR48k, True)
+          B.readFile ("opus_newvectors/testvector" <> file <> ".bit") >>= decodeFile decoderCfg >>= B.writeFile "tmp.out"
+          -- Use readProcessWithExitCode to account for the fact that opus_compare
+          -- returns a non-zero exit code if the comparing fails.
+          (exitcode1, stdout1, error1) <- readProcessWithExitCode "./opus_compare"
+            [ "-s"
+            , "-r", "48000"
+            , "opus_newvectors/testvector" <> file <> ".dec"
+            , "tmp.out"
+            ] ""
+          (exitcode2, stdout2, error2) <- readProcessWithExitCode "./opus_compare"
+            [ "-s"
+            , "-r", "48000"
             , "opus_newvectors/testvector" <> file <> "m.dec"
             , "tmp.out"
             ] ""
